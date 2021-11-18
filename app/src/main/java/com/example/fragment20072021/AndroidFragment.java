df49d9c3d6b70e88e382a2e0224374ec7503aef0
phamtanphat <phatdroid94@gmail.com>
2021-11-18 18:53:44 +0700

    activity send data to fragment

--- app/src/main/java/com/example/fragment20072021/AndroidFragment.java
+++ app/src/main/java/com/example/fragment20072021/AndroidFragment.java
@@ -1,19 +1,41 @@
 package com.example.fragment20072021;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class AndroidFragment extends Fragment {
+import com.example.fragment20072021.databinding.FragmentAndroidBinding;
+
+public class AndroidFragment extends Fragment implements DefaultLifecycleObserver {
+
+    FragmentAndroidBinding mBinding;
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        requireActivity().getLifecycle().addObserver(this);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_android, container, false);
+        mBinding = FragmentAndroidBinding.inflate(inflater,container,false);
+        Bundle bundle = getArguments();
+        if (bundle != null){
+            String text = bundle.getString("text");
+            mBinding.textViewFragmentAndroid.setText(text);
+        }
+        return mBinding.getRoot();
     }
+
+
 }
--- app/src/main/java/com/example/fragment20072021/MainActivity.java
+++ app/src/main/java/com/example/fragment20072021/MainActivity.java
@@ -9,14 +9,24 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager mFragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFragmentManager = getSupportFragmentManager();
+
     }
 
     public void addAndroid(View view) {
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("text","Hello Fragment Android");
+        androidFragment.setArguments(bundle);
+        fragmentTransaction.add(R.id.linearlayoutContainer,androidFragment);
+        fragmentTransaction.commitNow();
     }
 
     public void addIos(View view) {
